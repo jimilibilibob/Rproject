8a421f646e74bf65273ecd64e55d5dbcf135cb0a
thomas <thomas.fontaine95@live.fr>
2018-12-11 11:49:39 +0100

    Ajout repartition sexe age

--- proportion_sexe_age.R
+++ proportion_sexe_age.R
@@ -35,15 +35,17 @@ for(i in c(1:nrow(clients_r))){
     }
   }
 }
+
+data <- data.frame(sexe = sexe,age = age, age_group = age_group)
+
 row_data <- as.numeric(nrow(data))
 
 age_group <- cut(age,seq(18,98,10), include.lowest= TRUE, right = FALSE)
 limits <- seq(row_data*-1, row_data, by=row_data/50)
-labels <- seq(-100, 100, by=2)
+labels <- c(seq(100, 0, by=-2),seq(0, 100, by=2))
 data_femme <- subset(data,sexe=="femme")
 data_homme <- subset(data,sexe=="homme")
   
-data <- data.frame(sexe = sexe,age = age, age_group = age_group)
 
 gg_par_sexe <-  ggplot(data) +
   aes(x=age_group,fill=sexe) +
@@ -64,11 +66,11 @@ gg_global<-  ggplot(data) +
   aes(x=age_group,fill=sexe) +
   
   geom_bar(data = subset(data,sexe=="femme"),aes(y=((..count..)/row_data*-100) )) + 
-  geom_text(data = subset(data,sexe=="femme"), stat = "count", aes(label = round(..count../row_data*100,2), y = (..count..)/row_data*-100))+
+  geom_text(data = subset(data,sexe=="femme"), stat = "count", aes(label = round(..count../row_data*100,2), y = (..count..)/row_data*-100),  hjust = -0.01)+
   
   
   geom_bar(data = subset(data,sexe=="homme"),aes(y = (..count..)/row_data*100)) +
-  geom_text(data = subset(data,sexe=="homme"),stat = "count", aes(label = round(..count../row_data*100,2), y = (..count..)/row_data*100))+
+  geom_text(data = subset(data,sexe=="homme"),stat = "count", aes(label = round(..count../row_data*100,2), y = (..count..)/row_data*100),  hjust = -0.4)+
   
   scale_fill_manual(values = c("pink","blue")) + 
 
