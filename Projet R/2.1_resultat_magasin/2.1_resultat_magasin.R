c0c2bfabfe2174f6098833b15973113ef9a28687
thomas <thomas.fontaine95@live.fr>
2018-12-26 20:44:11 +0100

    Conformité 2.1 par rapport au Parametre.R

--- Projet R/2.1_resultat_magasin/2.1_resultat_magasin.R
+++ Projet R/2.1_resultat_magasin/2.1_resultat_magasin.R
@@ -4,7 +4,7 @@ nombre_client_magasin <- clients %>% group_by(MAGASIN) %>%   summarise(nombreCli
 
 resultat <- merge(x=magasins, y=nombre_client_magasin, by.x="CODESOCIETE", by.y="MAGASIN")
 
-activite_magasin <- entetes %>% group_by(MAG_CODE,IDCLIENT,year = format(as.Date(entetes_tickets$TIC_DATE),'%Y'))  
+activite_magasin <- entetes %>% group_by(MAG_CODE,IDCLIENT,year = format(as.Date(entetes$TIC_DATE),'%Y'))  
 
 activite_magasin_n2 <- subset(activite_magasin, activite_magasin$year == '2016') %>% group_by(MAG_CODE) %>% summarise(clientActifN2 = n())
 
@@ -17,7 +17,7 @@ resultat <- merge(x=resultat, y=activite_magasin_n1, by.x="CODESOCIETE", by.y="M
 resultat$evolutionClientActif <- round(resultat$clientActifN2/resultat$clientActifN1 * 100 -100,2)
 
 
-TOTALTCC_magasin <- entetes_tickets %>% group_by(MAG_CODE,year = format(as.Date(entetes_tickets$TIC_DATE),'%Y'))  %>%   summarise(TOTAL_TTC = sum(TIC_TOTALTTC))
+TOTALTCC_magasin <- entetes %>% group_by(MAG_CODE,year = format(as.Date(entetes$TIC_DATE),'%Y'))  %>%   summarise(TOTAL_TTC = sum(TIC_TOTALTTC))
 
 TOTALTTC_magasin_n2 <- subset(TOTALTCC_magasin, TOTALTCC_magasin$year == '2016') %>% summarise(TOTAL_TTCN1 = TOTAL_TTC )
 
