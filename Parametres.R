aa7a90e1e033bcad61b39d48244aed100d966213
Nicolas Robin <nicolas.robin@njrworld.com>
2018-12-28 17:39:26 +0100

    Rajout d'un booléen pour choisir de lire les fichiers "extrait" moins volumineux. Rajout d'une fonction qui permet à l'utilisateur de sélectionner le dossier où se trouvent les fichiers d'entrée.

--- Parametres.R
+++ Parametres.R
@@ -1,3 +1,14 @@
+# ---------------------------------------------------------------------------------------
+#      Fichier : Parametres.R
+# Développeurs : Thomas FONTAINE, Dan GOLDMAN, Juliette LEMAINS, Nicolas ROBIN
+#         Date : 28 décembre 2018.
+#  Description : Ce script inclut :
+#                 - Le chargement de l'ensemble des libraries nécessaires au projet.
+#                 - La déclaration du répertoires de travail et des fichiers
+#                 - Le chargement des fichiers d'entrée.
+#
+# ---------------------------------------------------------------------------------------
+
 # Chargement des libraries.
 require(assertthat)
 require(data.table)
@@ -7,68 +18,210 @@ require(ggplot2)
 require(plotly)
 require(rAmCharts)
 require(stringr)
+require(tcltk)
 
-# Declaration du répertoire de travail.
-chemin_access_environnement_du_projet <- "/Users/nrobin/Documents/GitHub/Rproject/"
 
-# Declaration du chemin du dossier dans lequel se trouve les fichiers de donnees.
-chemin_dossier_donnees <- paste0(chemin_access_environnement_du_projet, "DATA/")
+# La variable booléene "utilise_des_extraits_de_fichier" permet de sélectionner des extraits de fichiers
+# plutôt que les fichiers d'entrée volumineux du projet. Cela permet de travailler sur des machines moins véloces.
+# Mettre la valeur à TRUE lorsque l'on désire travailler avec des fichiers plus petits.
+# Ne pas oublier de mettre cette valeur à FALSE lorsque le projet est finalisé.
+utilise_des_extraits_de_fichier <- FALSE
+
+# Chemins de dossier des fichiers de donnée respectifs dans le cas où on n'utilise pas la boite de dialogue.
+chemin_dossier_donnees_Thomas <- "..."
+chemin_dossier_donnees_Dan <- "..."
+chemin_dossier_donnees_Juliette <- "..."
+chemin_dossier_donnees_Nicolas <- "/Users/nrobin/Documents/GitHub/Rproject/DATA/"
+
+# Fonction "dossierFichiersDonnees".
+# Cette fonction renvoie le chemin du dossier où se trouve les fichiers de données d'entrée.
+# Si aucun dossier n'est sélectionné, la fonction retourne "NA".
+cheminDossierFichiersDonnees <- function(chemin_dossier_donnees_par_defaut) {
+  
+  # Declaration du chemin du dossier dans lequel se trouve les fichiers de donnees.
+  chemin <- tk_choose.dir(getwd(), "Sélectionner le dossier où se trouvent les fichiers d'entrée :")
+  
+  if (is.na(chemin)) {
+    
+    chemin <- chemin_dossier_donnees_par_defaut
+    
+  }else{
+    # Ajout d'un "/" pour le dossier sélectionné par l'utilisateur.
+    chemin <- paste0(chemin, "/")
+  }
+  
+  # Si la valeur de "chemin_dossier_donnees" n'a pas été renseignée, on affiche un message d'erreur.
+  if (is.na(chemin)) {
+    tk_messageBox("ok", "Le dossier contenant les fichiers d'entrée n'a pas été sélectionné", caption = "Erreur", default = "")
+  }
+  
+  return(chemin)
+}
+
+# Affectation de la variable globale "chemin_dossier_donnees".
+# Cette valeur indique le chemin du dossier dans lequel se trouvent tous les fichiers de donnée.
+chemin_dossier_donnees <- cheminDossierFichiersDonnees(chemin_dossier_donnees_Nicolas)
 
 # La liste des fichiers de donnees necessaires au projet.
+nom_fichier_INSEE <- "correspondance-code-insee-code-postal.csv"
 nom_fichier_articles <- "REF_ARTICLE.CSV"
 nom_fichier_magasins <- "REF_MAGASIN.CSV"
-nom_fichier_clients <- "CLIENT.CSV"
-nom_fichier_entetes <- "ENTETES_TICKET_V4.CSV"
-nom_fichier_lignes <- "LIGNES_TICKET_V4.CSV"
-nom_fichier_INSEE <- "correspondance-code-insee-code-postal.csv"
 
-# Le chemin d'accès complet des fichiers de donnees.
-chemin_fichier_articles <- paste0(chemin_dossier_donnees, nom_fichier_articles)
-chemin_fichier_magasins <- paste0(chemin_dossier_donnees, nom_fichier_magasins)
-chemin_fichier_clients <- paste0(chemin_dossier_donnees, nom_fichier_clients)
-chemin_fichier_entetes <- paste0(chemin_dossier_donnees, nom_fichier_entetes)
-chemin_fichier_lignes <- paste0(chemin_dossier_donnees, nom_fichier_lignes)
-chemin_fichier_INSEE <- paste0(chemin_dossier_donnees, nom_fichier_INSEE)
+if (utilise_des_extraits_de_fichier == TRUE) {
+  nom_fichier_clients <- "SUBCLIENT.CSV"
+}else{
+  nom_fichier_clients <- "CLIENT.CSV"
+}
 
+if (utilise_des_extraits_de_fichier == TRUE) {
+  nom_fichier_entetes <- "SUBENTETE.CSV"
+}else{
+  nom_fichier_entetes <- "ENTETES_TICKET_V4.CSV"
+}
 
-# Chargement du fichier des articles en 2 secondes
-# articles <- read.csv(chemin_fichier_articles, sep="|", header=TRUE, skipNul = T, stringsAsFactors = FALSE)
-# Chargement du fichier des articles en 1 secondes
-articles <- fread(chemin_fichier_articles, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+if (utilise_des_extraits_de_fichier == TRUE) {
+  nom_fichier_lignes <- "SUBLIGNES.CSV"
+}else{
+  nom_fichier_lignes <- "LIGNES_TICKET_V4.CSV"
+}
 
-# Cette ligne de code retire la ligne COUPON.
-# Faut-il la retirer ou pas ? N'aura-t-on pas besoin de cette information plus tard ?
-#articles <- articles[-c(1),]
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableArticles"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableArticles <- function() {
+  
+  tableArticles <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_articles)
 
-# Chargement du fichier magasins en moins d'une seconde
-#magasins <- read.csv(chemin_fichier_magasins, sep="|", header=TRUE, skipNul = T, stringsAsFactors = FALSE)
-# Chargement du fichier magasins en moins d'une seconde
-magasins <- fread(chemin_fichier_magasins, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    # Chargement du fichier des articles en 1 secondes
+    tableArticles <- fread(chemin_fichier, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    
+    # Cette ligne de code retire la ligne COUPON.
+    # Faut-il la retirer ou pas ? N'aura-t-on pas besoin de cette information plus tard ?
+    # tableArticles <- tableArticles[-c(1),]
+  }
+  
+  returnValue(tableArticles)
+}
 
-# Chargement du fichier clients en 6 secondes
-#clients <- read.csv(chemin_fichier_clients, sep="|", header=TRUE, skipNul = T, stringsAsFactors = FALSE)
-# Chargement du fichier magasins en 2 secondes
-clients <- fread(chemin_fichier_clients, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
-clients[clients == ""] <- NA # On met tous les clients vides à "NA".
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableMagasins"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableMagasins <- function() {
+  
+  tableMagasins <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_magasins)
+    
+    # Chargement du fichier des articles en 1 secondes
+    tableMagasins <- fread(chemin_fichier, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    
+  }
 
-# Chargement du fichier entetes en 30 secondes.
-#entetes <- read.csv(chemin_fichier_entetes, sep="|", header=TRUE, skipNul = T, stringsAsFactors = FALSE)
-# Chargement du fichier entetes en 12 secondes.
-entetes <- fread(chemin_fichier_entetes, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+  returnValue(tableMagasins)
+}
 
-# Chargement du fichier magasins en 120 secondes
-#lignes <- read.csv(chemin_fichier_lignes, sep="|", header=TRUE, skipNul = T)
-# Chargement du fichier magasins en 30 secondes
-lignes <- fread(chemin_fichier_lignes, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableClients"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableClients <- function() {
+  
+  tableClients <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_clients)
+    
+    # Chargement du fichier des articles en 1 secondes
+    tableClients <- fread(chemin_fichier, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    tableClients[tableClients == ""] <- NA
+    
+  }
+  
+  returnValue(tableClients)
+}
 
-#------------------------------------------------------------------------------------
-# LECTURE DU FICHIER PUBLIC OPEN DATA DE L'INSEE.
-#------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableEntetes"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableEntetes <- function() {
+  
+  tableEntetes <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_entetes)
+    
+    # Chargement du fichier des articles en 1 secondes
+    tableEntetes <- fread(chemin_fichier, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    
+  }
+  
+  returnValue(tableEntetes)
+}
 
-# Il s'agit d'un fichier de correspondance entre le code-insee, le code-postal, la longitude et la latitude.
-# Thèmes : Administration, Gouvernement, Finances publiques, Citoyennete
-# Licence : Licence Ouverte (Etalab)
-# Langue : Français
-# Dernière date de mise à jour : 22 avril 2016 16:47
-# Producteur : OpenDataSoft
-INSEE_CP_FULL_DATATABLE <- fread(chemin_fichier_INSEE, sep = ";", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableLignes"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableLignes <- function() {
+  
+  tableLignes <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_lignes)
+    
+    # Chargement du fichier des articles en 1 secondes
+    tableLignes <- fread(chemin_fichier, sep="|", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    
+  }
+  
+  returnValue(tableLignes)
+}
+
+# ------------------------------------------------------------------------------------
+# Fonction "chargementTableInsee"
+# Cette fonction charge la table dans la mesure où le dossier a été sélectionné.
+# ------------------------------------------------------------------------------------
+chargementTableInsee <- function() {
+  
+  tableInsee <- NULL
+  
+  if (!is.na(chemin_dossier_donnees)) {
+    
+    # Le chemin d'accès complet des fichiers de donnees.
+    chemin_fichier <- paste0(chemin_dossier_donnees, nom_fichier_INSEE)
+    
+    # Chargement du fichier des articles en 1 secondes
+    tableInsee <- fread(chemin_fichier, sep=";", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+    
+  }
+  
+  returnValue(tableInsee)
+}
+
+# Chargement des 5 tables à utiliser en entrée plus la table Insee.
+# Les données peuvent maintenant être traitées pour chacun dees exercices du projet.
+articles <- chargementTableArticles()
+magasins <- chargementTableMagasins()
+clients <- chargementTableClients()
+entetes <- chargementTableEntetes()
+lignes <- chargementTableLignes()
+insee <- chargementTableInsee()
+
+  
